d05e4b011616a28e8fc5623124c86d695ee18ef7
jagadw <jagadhitawicaksana@gmail.com>
2023-10-23 21:53:06 +0800

    Mengganti enkripsi recovery dari MD5 menjadi password_hash

--- recovery/proses.php
+++ recovery/proses.php
@@ -2,16 +2,23 @@
     include '../koneksi.php';
     $username = $_POST['username'];
     $query = mysqli_query($koneksi, "SELECT * FROM tb_login WHERE username='$username' LIMIT 1");
+
+    $query_username = mysqli_query($koneksi, "SELECT COUNT(*) FROM tb_login WHERE username='$username'");
+    $check_username = mysqli_fetch_row($query_username);
+
     if(mysqli_num_rows($query) > 0){
-        $password = md5($_POST['password']);
-        $re_password = md5($_POST['re_password']);
+        $password = $_POST['password'];
+        $pass_hash = password_hash($password, PASSWORD_DEFAULT); // meng-enkripsi password
+        $re_password = $_POST['re_password'];
         if ($password == $re_password) {
-            $update = mysqli_query($koneksi, "UPDATE tb_login SET password='$password' WHERE username='$username'");
+            $update = mysqli_query($koneksi, "UPDATE tb_login SET password='$pass_hash' WHERE username='$username'");
             header('location: ../index.php?pesan=berhasil_update');
-        }else{
-            header('location: index.php?pesan=invalid_password');
+        } else if($password != $re_password) {
+            echo "<script>alert('Password tidak sesuai');window.location.href='index.php'</script>";
         }
+    }else if($check_username['0'] == 0){
+        echo "<script>alert('Username tersebut tidak terdaftar');window.location.href='index.php'</script>";
     }else{
-        header('location: index.php?pesan=gagal_update');
+        echo "<script>alert('Gagal mengubah password');window.location.href='index.php'</script>";
     }
 ?>
